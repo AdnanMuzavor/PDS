437675b89de4af01ac7878f651b10f44a775e538
Adnan Muzavor <www.adnan9168389872@gmail.com>
2022-01-13 17:14:54 +0530

    LL-1

--- Linkedlist/Rotate_list.cpp
+++ Linkedlist/Rotate_list.cpp
@@ -1,7 +1,7 @@
 /*
 Given the head of a linked list, rotate the list to the right by k places.
 
- 
+
 
 Example 1:
 
@@ -28,41 +28,58 @@ using namespace std;
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution
+{
+    struct ListNode
+    {
+        int val;
+        ListNode *next;
+        ListNode() : val(0), next(nullptr) {}
+        ListNode(int x) : val(x), next(nullptr) {}
+        ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+
 public:
-    int length(ListNode* h){
-        ListNode*p=h;
-        int l=0;
-        while(p!=NULL){
-            p=p->next;
+    int length(ListNode *h)
+    {
+        ListNode *p = h;
+        int l = 0;
+        while (p != NULL)
+        {
+            p = p->next;
             l++;
         }
         return l;
     }
-    ListNode* rotateRight(ListNode* head, int k) {
-        if(length(head)==1 || length(head)==0 || k==0 ||k==length(head)) return head;
-        k=k%length(head);
-        if(k==0) return head;
-        int l=length(head)-k;
-        ListNode *p=NULL,*q=NULL; 
-        //q for pointing last node and p for pointing new head
-        p=head;
-        while(l--){
-            q=p;
-            p=p->next;
+    ListNode *rotateRight(ListNode *head, int k)
+    {
+        if (length(head) == 1 || length(head) == 0 || k == 0 || k == length(head))
+            return head;
+        k = k % length(head);
+        if (k == 0)
+            return head;
+        int l = length(head) - k;
+        ListNode *p = NULL, *q = NULL;
+        // q for pointing last node and p for pointing new head
+        p = head;
+        while (l--)
+        {
+            q = p;
+            p = p->next;
         }
-       // cout<<q->val<<" "<<p->val<<endl;
-        q->next=NULL;
-        
-        //To traverse till end of detached LL
-        ListNode*r=p;
-        while(r->next!=NULL) r=r->next;
-        
-        //Attaching last node of detached LL with old head
-        r->next=head;
-        
-        //Getting new head
-        head=p;
+        // cout<<q->val<<" "<<p->val<<endl;
+        q->next = NULL;
+
+        // To traverse till end of detached LL
+        ListNode *r = p;
+        while (r->next != NULL)
+            r = r->next;
+
+        // Attaching last node of detached LL with old head
+        r->next = head;
+
+        // Getting new head
+        head = p;
         return head;
     }
 };
