ce6cfc0e2a5ba5bcd256b4441130d1591817cd8b
Adnan Muzavor <www.adnan9168389872@gmail.com>
2022-01-24 22:09:52 +0530

    dp -3

---
+++ Recursion/permutation_II.cpp
@@ -0,0 +1,102 @@
+/*
+47. Permutations II
+Medium
+
+4317
+
+89
+
+Add to List
+
+Share
+Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+
+
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output:
+[[1,1,2],
+ [1,2,1],
+ [2,1,1]]
+Example 2:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// BRUTE FORCE APPROACH
+class Solution
+{
+public:
+    vector<vector<int>> permuteUnique(vector<int> &nums)
+    {
+        vector<vector<int>> op;
+        if (nums.size() == 1)
+        {
+            op.push_back(nums);
+            return op;
+        }
+        perm(nums, 0, op);
+        return op;
+    }
+    void perm(vector<int> v, int begin, vector<vector<int>> &op)
+    {
+        if (begin >= v.size())
+        {
+            if (find(op.begin(), op.end(), v) == op.end())
+            {
+                op.push_back(v);
+            }
+            return;
+        }
+        for (int i = begin; i < v.size(); i++)
+        {
+            swap(v[begin], v[i]);
+            perm(v, begin + 1, op);
+            swap(v[i], v[begin]);
+        }
+    }
+};
+
+// Better Approach(not best)
+
+class Solution
+{
+public:
+    map<vector<int>, int> m;
+    vector<vector<int>> permuteUnique(vector<int> &nums)
+    {
+        vector<vector<int>> op;
+        if (nums.size() == 1)
+        {
+            op.push_back(nums);
+            return op;
+        }
+        perm(nums, 0, op);
+        return op;
+    }
+    void perm(vector<int> v, int begin, vector<vector<int>> &op)
+    {
+        if (begin >= v.size())
+        {
+
+            if (!m[v])
+            {
+                m[v]++;
+                op.push_back(v);
+            }
+            return;
+        }
+        for (int i = begin; i < v.size(); i++)
+        {
+
+            swap(v[begin], v[i]);
+            perm(v, begin + 1, op);
+            swap(v[i], v[begin]);
+        }
+    }
+};
