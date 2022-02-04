1f5abddadef071d33d9fe271bee018ae80c4621c
Adnan Muzavor <www.adnan9168389872@gmail.com>
2022-02-04 12:02:56 +0530

    array 37

---
+++ Array/3_sum.cpp
@@ -0,0 +1,32 @@
+/*
+15. 3Sum
+Medium
+
+15786
+
+1513
+
+Add to List
+
+Share
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Example 2:
+
+Input: nums = []
+Output: []
+Example 3:
+
+Input: nums = [0]
+Output: []
+*/
+#include<bits/stdc++.h>
+using namespace std;
