6ec7c3f40be346ab7ca4721630ab652c35494c8e
Adnan Muzavor <www.adnan9168389872@gmail.com>
2022-02-08 12:54:34 +0530

    array 39

--- Array/combinations.cpp
+++ Array/combinations.cpp
@@ -35,7 +35,88 @@ Output: [[1]]
 
 #include <bits/stdc++.h>
 using namespace std;
+// Optimised solution
+class Solution
+{
+public:
+    // Better then Brute force
+    // unordered_map<int,int> m;
+    vector<vector<int>> op;
+    void perm(vector<int> curr, int val, int k, int start)
+    {
+        if (curr.size() >= k)
+        {
+            // If size becomes >=k i.e ==k push into op and return
+            op.push_back(curr);
+            return;
+        }
+        for (int i = start; i <= val; i++)
+        {
+            // To avoid duplicate pairing
+            // Push current elemet , to avoid duplicate while calling fn when pass starting value as i+1
+            // i.e given k=2
+            // First time curr[1]
+            // Calling permut([1],4,2,2)
+            // Again into this loop but loop starts with given satrt index i.e 2
+            // so it becomes[1,2], thn 2 popped, [1,3] , 3 popped [1,4]
+            // Then we come back to first call with i=2 and curr=[]
+            // We push 2 into curr[2]
+            // We call permute([2],4,2,3)
+            // Again since size is small we loop
+            //[2,3] pop 3, [2,4] , pop 4
+            // Again come back to first call ,and make curr[3] and so on
+            curr.push_back(i);
+            // m[i]++;
+            perm(curr, val, k, i + 1);
+            // m[i]--;
+            curr.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k)
+    {
 
+        vector<int> curr;
+        perm(curr, n, k, 1);
+
+        return op;
+    }
+};
+class Solution
+{
+public:
+    // Better then Brute force
+    set<vector<int>> op;
+    void perm(vector<int> curr, int val, int k)
+    {
+        if (curr.size() >= k)
+        {
+            sort(curr.begin(), curr.end());
+            op.insert(curr);
+            return;
+        }
+        for (int i = 1; i <= val; i++)
+        {
+            // To avoid duplicate pairing
+            if (find(curr.begin(), curr.end(), i) == curr.end())
+            {
+                curr.push_back(i);
+                perm(curr, val, k);
+                curr.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> combine(int n, int k)
+    {
+        vector<vector<int>> op1;
+        vector<int> curr;
+        perm(curr, n, k);
+        for (auto e : op)
+        {
+            op1.push_back(e);
+        }
+        return op1;
+    }
+};
 // BRUTE FORCE
 class Solution
 {
